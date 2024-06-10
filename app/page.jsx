3582f4e7c91542a92a537839d535c184fc5efe88
Nishant Choubisa <116937601+nishabdShunya@users.noreply.github.com>
2024-06-10 10:17:37 +0530

    Initial Setup and Heading

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="text-center head_text">
+        Discover & Share
+        <br className="md:hidden" />
+        <span className="text-center orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="text-center desc">
+        Promptopia is an open source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
+
+      {/* Feed */}
+    </section>
+  );
+};
+
+export default Home;
